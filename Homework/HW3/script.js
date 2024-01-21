bbfa9687ec05fcb5c74c6fa9929ab77043659cbf
estaciwo <staciwo@gmail.com>
2024-01-21 14:47:56 -0800

    Add files via upload

---
+++ Homework/HW3/script.js
@@ -0,0 +1,23 @@
+let grades = "jim|25, sue|32, mary|34, ann|22, ted|28, frank|15, lisa|19, mike|30, ahn|26, vishaya|27";
+let gradeArray = grades.split(", ");
+let total = 0;
+let avg = 0;
+let gradeNum = 0;
+let nuStudent = [];
+let nuGrades = [];
+let hiGrade = 0;
+let loGrade = 100;
+/* foreach GRADE in GRADEARRAY 
+
+
+    (do thing: take last two characters and convert to int GRADE
+
+    take everything before last three characters and assign to NAME
+
+
+    variable.sort() alphabetical, increasing numerical
+
+) */
+
+
+
